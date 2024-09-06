a538e7aabd657792c6d2b4ea0ee8fa37e18ae6fa
thomas.mccarthy@storm-technologies.com <thomas.mccarthy@storm-technologies.com>
2024-09-06 17:04:47 +0000

    add main.py and constains

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    started, failed = pygame.init()
+
+    print(f"started pygame modules: {started}")
+    print(f"failed to start: {failed}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    #surface = pygame.Surface()
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
