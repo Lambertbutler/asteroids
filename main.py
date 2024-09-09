cc1e69b467d5539ac089c021e4976f759236409a
thomas.mccarthy@storm-technologies.com <thomas.mccarthy@storm-technologies.com>
2024-09-09 18:27:34 +0000

    Added two sprite group, place above screen fill, to see if it draw over the player object

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
 
     print(f"started pygame modules: {started}")
     print(f"failed to start: {failed}")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
@@ -21,13 +25,22 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        
+        # update all objects
+        for obj in updatable:
+            obj.update(dt)
+
+        # draw all objects
+        for obj in drawable:
+            obj.draw(screen)
+
         screen.fill("black")
         
         player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
-        player.update(dt)
+        
 
 
 if __name__ == "__main__":
