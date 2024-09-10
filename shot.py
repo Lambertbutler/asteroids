798cb3b04b949988cb85796706bcd02743ddacee
thomas.mccarthy@storm-technologies.com <thomas.mccarthy@storm-technologies.com>
2024-09-10 11:38:43 +0000

    Fix Shot class, Missed Self in init method

--- shot.py
+++ shot.py
@@ -1,9 +1,9 @@
 import pygame
 from circleshape import CircleShape
-from constants import SHOT_RADIUS, PLAYER_SHOOT_SPEED
+from constants import SHOT_RADIUS
 
 class Shot(CircleShape):
-    def __init__(x,y):
+    def __init__(self,x,y):
         super().__init__(x,y,SHOT_RADIUS)
         
     def draw(self, screen):
