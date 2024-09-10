93b68cc122caf5e927ed8cb291c7e62136896871
thomas.mccarthy@storm-technologies.com <thomas.mccarthy@storm-technologies.com>
2024-09-10 12:48:12 +0000

    added time to live to Shot class, shots will die off after a 5 seconds

--- shot.py
+++ shot.py
@@ -5,10 +5,17 @@ from constants import SHOT_RADIUS
 class Shot(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,SHOT_RADIUS)
-        
+        self.ttl = 5
+        self.alive = 0
     def draw(self, screen):
        pygame.draw.circle(screen, "white", self.position, self.radius, width=2) 
 
+    def timeToLive(self):
+        if self.ttl <= self.alive:
+            self.kill()
+    
     def update(self,dt):
         self.wraparound()
+        self.timeToLive()
+        self.alive += dt
         self.position += self.velocity * dt   
